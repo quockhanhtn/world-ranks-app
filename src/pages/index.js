4086100ae8ade87270be1d80e8a03c57dd280a58
Khanh Lam <quockhanhdev@gmail.com>
2021-07-13 20:29:39 +0700

    Call API using getStaticProps()

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
 import Layout from '../components/Layout/Layout';
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return (
-    <Layout>main section</Layout>
+    <Layout>
+      <div>Found {countries.length} countries</div>
+    </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+  return {
+    props: {
+      countries
+    }
+  };
 }
